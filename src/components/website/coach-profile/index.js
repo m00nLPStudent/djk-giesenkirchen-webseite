d4f737605aba08202bbe595099e112aa2f5de7b6
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 21:34:03 +0200

    Export coach contact actions

--- src/components/website/coach-profile/index.js
+++ src/components/website/coach-profile/index.js
@@ -1,4 +1,5 @@
 export { default as CoachProfileImageCard } from "./CoachProfileImageCard";
 export { default as CoachProfileHeader } from "./CoachProfileHeader";
 export { default as CoachProfileStatsGrid } from "./CoachProfileStatsGrid";
+export { default as CoachProfileContactActions } from "./CoachProfileContactActions";
 export * from "./coachProfile.helpers";
