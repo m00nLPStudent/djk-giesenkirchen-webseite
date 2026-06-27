e1a07fd5399d6e528bbcfc9e8904542af4539370
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-28 00:20:07 +0200

    Export shared settings fields

--- src/components/admin/forms/index.js
+++ src/components/admin/forms/index.js
@@ -1,4 +1,5 @@
 export * from "./FormField";
 export * from "./FormSection";
 export * from "./SpecialFields";
+export * from "./SettingsFields";
 export { default as StatusSwitch } from "./StatusSwitch";
