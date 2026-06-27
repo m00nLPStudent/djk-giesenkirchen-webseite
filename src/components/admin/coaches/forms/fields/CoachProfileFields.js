043c33308251ce80cf8c185d8c8f408186549713
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 22:03:16 +0200

    Polish coach nationality field

--- src/components/admin/coaches/forms/fields/CoachProfileFields.js
+++ src/components/admin/coaches/forms/fields/CoachProfileFields.js
@@ -4,11 +4,13 @@ import { SelectInput } from "./CoachFormField";
 export default function CoachProfileFields({ form, errors, updateField }) {
   return (
     <SelectInput
+      label="Nationalität"
+      required
       value={form.nationality}
       onChange={(event) => updateField("nationality", event.target.value)}
       error={errors.nationality}
     >
-      <option value="">Nationalität auswählen *</option>
+      <option value="">Nationalität auswählen</option>
       {COACH_COUNTRY_OPTIONS.map((country) => (
         <option key={country.iso} value={country.iso}>
           {country.flag} {country.de}
