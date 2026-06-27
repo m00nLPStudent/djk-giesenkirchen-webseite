3df9bf07c3ec12c72135e28564935a13b8537d66
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 20:46:42 +0200

    Add coach placeholder image constant

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,2 +1,4 @@
 export const PLAYER_PLACEHOLDER_IMAGE =
   "https://dbiwxylqbkxpkwkfcjut.supabase.co/storage/v1/object/public/media/players/Blanko.png";
+
+export const COACH_PLACEHOLDER_IMAGE = PLAYER_PLACEHOLDER_IMAGE;
