c05160b55769d0fd44a915d4d54d3a48430f9508
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 15:07:32 +0200

    Add players module exports

---
+++ src/components/admin/players/index.js
@@ -0,0 +1,9 @@
+export { default as AdminPlayersList } from "./AdminPlayersList";
+export { default as AdminPlayersForm } from "./forms/AdminPlayersForm";
+
+export { default as PlayerStats } from "./components/PlayerStats";
+export { default as PlayerCard } from "./components/PlayerCard";
+export { default as PlayerFilters } from "./components/PlayerFilters";
+export { default as PlayerEmptyState } from "./components/PlayerEmptyState";
+export { default as PlayerImageUpload } from "./components/PlayerImageUpload";
+export { default as PlayerStatusBadge } from "./components/PlayerStatusBadge";
