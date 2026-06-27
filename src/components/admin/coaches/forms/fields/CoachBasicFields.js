61a964425aa469dd528d7fe775d01024282c63cf
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 22:02:01 +0200

    Polish coach basic fields layout

--- src/components/admin/coaches/forms/fields/CoachBasicFields.js
+++ src/components/admin/coaches/forms/fields/CoachBasicFields.js
@@ -1,30 +1,35 @@
+import { FormGrid } from "@/components/admin/forms";
 import { TextInput } from "./CoachFormField";
 
 export default function CoachBasicFields({ form, errors, updateField }) {
   return (
-    <>
-      <div className="grid gap-4 md:grid-cols-2">
+    <div className="space-y-4">
+      <FormGrid>
         <TextInput
-          placeholder="Vorname *"
+          label="Vorname"
+          required
+          placeholder="Vorname"
           value={form.first_name}
           onChange={(event) => updateField("first_name", event.target.value)}
           error={errors.first_name}
         />
 
         <TextInput
-          placeholder="Nachname *"
+          label="Nachname"
+          required
+          placeholder="Nachname"
           value={form.last_name}
           onChange={(event) => updateField("last_name", event.target.value)}
           error={errors.last_name}
         />
-      </div>
+      </FormGrid>
 
-      <input
-        placeholder="Slug, z. B. swen-verbocket"
+      <TextInput
+        label="Slug"
+        placeholder="z. B. swen-verbocket"
         value={form.slug}
         onChange={(event) => updateField("slug", event.target.value)}
-        className="h-14 w-full rounded-2xl border border-white/10 bg-white/5 px-4 outline-none focus:border-red-500"
       />
-    </>
+    </div>
   );
 }
