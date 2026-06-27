18ea693d43e00655a8982b3a2f67a05dfe8aa98a
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 20:15:01 +0200

    Export player profile components

---
+++ src/components/website/player-profile/index.js
@@ -0,0 +1,5 @@
+export { default as PlayerProfileImageCard } from "./PlayerProfileImageCard";
+export { default as PlayerProfileHeader } from "./PlayerProfileHeader";
+export { default as PlayerProfileStatsGrid } from "./PlayerProfileStatsGrid";
+export { default as PlayerProfileDescription } from "./PlayerProfileDescription";
+export * from "./playerProfile.helpers";
