d289ca9c83668ad27608491c76f25e1d12dae42f
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 21:24:25 +0200

    Export coach profile components

---
+++ src/components/website/coach-profile/index.js
@@ -0,0 +1,4 @@
+export { default as CoachProfileImageCard } from "./CoachProfileImageCard";
+export { default as CoachProfileHeader } from "./CoachProfileHeader";
+export { default as CoachProfileStatsGrid } from "./CoachProfileStatsGrid";
+export * from "./coachProfile.helpers";
