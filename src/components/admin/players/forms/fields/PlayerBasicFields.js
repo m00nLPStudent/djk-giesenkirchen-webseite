8829b8a7b9f4f363d76ea909c08edea9b824b5c6
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-27 22:17:55 +0200

    Polish player basic fields with shared form styling

--- src/components/admin/players/forms/fields/PlayerBasicFields.js
+++ src/components/admin/players/forms/fields/PlayerBasicFields.js
@@ -1,31 +1,38 @@
+import { FormGrid } from "@/components/admin/forms";
 import { InputField, SelectField } from "./FormField";
 
 export default function PlayerBasicFields({ form, errors, teams, updateField }) {
   return (
-    <>
-      <div className="grid gap-4 md:grid-cols-2">
+    <div className="space-y-4">
+      <FormGrid>
         <InputField
-          placeholder="Vorname *"
+          label="Vorname"
+          required
+          placeholder="Vorname"
           value={form.first_name}
           onChange={(event) => updateField("first_name", event.target.value)}
           error={errors.first_name}
         />
 
         <InputField
-          placeholder="Nachname *"
+          label="Nachname"
+          required
+          placeholder="Nachname"
           value={form.last_name}
           onChange={(event) => updateField("last_name", event.target.value)}
           error={errors.last_name}
         />
-      </div>
+      </FormGrid>
 
-      <div className="grid gap-4 md:grid-cols-2">
+      <FormGrid>
         <SelectField
+          label="Mannschaft"
+          required
           value={form.team_id}
           onChange={(event) => updateField("team_id", event.target.value)}
           error={errors.team_id}
         >
-          <option value="">Mannschaft auswählen *</option>
+          <option value="">Mannschaft auswählen</option>
           {teams.map((team) => (
             <option key={team.id} value={team.id}>
               {team.name_de}
@@ -34,12 +41,13 @@ export default function PlayerBasicFields({ form, errors, teams, updateField }) 
         </SelectField>
 
         <InputField
+          label="Rückennummer"
           type="number"
-          placeholder="Rückennummer"
+          placeholder="z. B. 10"
           value={form.shirt_number}
           onChange={(event) => updateField("shirt_number", event.target.value)}
         />
-      </div>
-    </>
+      </FormGrid>
+    </div>
   );
 }
