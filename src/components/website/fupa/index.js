3a57fb875ff334c736c49aa584fd6d4ddb9d90ff
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-28 20:39:19 +0200

    Export FuPa module components

--- src/components/website/fupa/index.js
+++ src/components/website/fupa/index.js
@@ -1,2 +1,6 @@
+export { default as FupaCard } from "./FupaCard";
+export { default as FupaError } from "./FupaError";
+export { default as FupaLoader } from "./FupaLoader";
 export { default as FupaScript } from "./FupaScript";
+export { default as FupaSection } from "./FupaSection";
 export { default as FupaWidget } from "./FupaWidget";
