b2f751c46d7a4f40580a3a936db78d64b9771d72
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-29 19:40:22 +0200

    Export football.de website components

---
+++ src/components/website/football-de/index.js
@@ -0,0 +1,5 @@
+export { default as FootballDeCard } from "./FootballDeCard";
+export { default as FootballDeError } from "./FootballDeError";
+export { default as FootballDeScript } from "./FootballDeScript";
+export { default as FootballDeSection } from "./FootballDeSection";
+export { default as FootballDeWidget } from "./FootballDeWidget";
