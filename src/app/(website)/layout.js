31c212b1f3e616440d7ce81f882110e027e49936
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-06-29 19:47:58 +0200

    Load football.de widget styles

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import "@/styles/fupa-widget.css";
+import "@/styles/football-de-widget.css";
 
 export default function WebsiteLayout({ children }) {
   return (
