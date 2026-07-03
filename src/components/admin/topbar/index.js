9bc5b1d7214f3396a1db3ffc42f52bfbce596278
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-07-03 22:13:17 +0200

    Add admin topbar exports

---
+++ src/components/admin/topbar/index.js
@@ -0,0 +1,6 @@
+export { default as AdminTopbar } from "./AdminTopbar";
+export { default as AdminSearch } from "./AdminSearch";
+export { default as AdminTopbarClock } from "./AdminTopbarClock";
+export { default as NotificationBell } from "./NotificationBell";
+export { default as ProfileMenu } from "./ProfileMenu";
+export { default as WebsiteButton } from "./WebsiteButton";
