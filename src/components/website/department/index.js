8d329ea9f190955ff4422846848fdd1aaaab5d34
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-07-03 20:34:40 +0200

    Add department component exports

---
+++ src/components/website/department/index.js
@@ -0,0 +1,4 @@
+export { default as DepartmentPageLayout } from "./DepartmentPageLayout";
+export { default as DepartmentPersonCard } from "./DepartmentPersonCard";
+export { default as DepartmentPersonGrid } from "./DepartmentPersonGrid";
+export { getCoachTeamName, mapBoardMemberForDisplay } from "./department.helpers";
