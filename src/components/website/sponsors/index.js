1f0a61601d51cb32649498371131cca9e1b290ea
Swen J. Verbocket <102254700+m00nLPStudent@users.noreply.github.com>
2026-07-03 21:54:25 +0200

    Export sponsor subcomponents

--- src/components/website/sponsors/index.js
+++ src/components/website/sponsors/index.js
@@ -1,3 +1,6 @@
+export { default as SponsorActions } from "./SponsorActions";
+export { default as SponsorBanner } from "./SponsorBanner";
 export { default as SponsorCard } from "./SponsorCard";
+export { default as SponsorGrid } from "./SponsorGrid";
 export { default as SponsorSection } from "./SponsorSection";
 export { default as SponsorTabs } from "./SponsorTabs";
